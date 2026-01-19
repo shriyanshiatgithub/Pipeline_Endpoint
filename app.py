c92c682251fff869ead2a41c55fcd3312d0f8e31
Shriyanshi Soni <shriyanshisoni.com>
2026-01-19 07:22:02 +0000

    initial commit with alll files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({
+        "status": "UP",
+        "message": "End point creation successfull"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
